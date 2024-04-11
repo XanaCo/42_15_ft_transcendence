582ff9b9dcffd7d712ab42b955982fa8f439bcc3
pcheron-root/42_ft_transcendance <pablo.cheron@proton.me>
2024-04-11 09:47:17 +0200

    fix: Makefile part 3

--- services/backend/arena/tools/arenaGame/arena/rooting.py
+++ services/backend/arena/tools/arenaGame/arena/rooting.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import battle
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<room_name>\w+)/$", battle.Challenger.as_asgi()),
+    re_path("ws/challenger/", battle.Challenger.as_asgi()),
 ]
--- services/nginx/frontend/js/arena.js
+++ services/nginx/frontend/js/arena.js
@@ -1,22 +1,51 @@
-// Création d'une nouvelle instance de WebSocket en spécifiant l'URL de votre serveur Django
-const socket = new WebSocket('ws://localhost:8000/challenger/');
 
-// Événement déclenché lorsqu'une connexion WebSocket est établie
-socket.onopen = function(event) {
-    console.log('WebSocket Connected');
-};
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: ["http://localhost:8080"],
+    },
+})
 
-// Événement déclenché lorsqu'un message est reçu du serveur
-socket.onmessage = function(event) {
-    console.log('Message from server:', event.data);
-    // Traitez le message reçu du serveur selon vos besoins
-};
+io.on("connection", socket => {
+    console.log(socket.id)
+})
 
-// Fonction pour envoyer un message au serveur
-function sendMessage(message) {
-    socket.send(JSON.stringify({"message": message}));
-}
+// Handle arrows
+document.addEventListener('keydown', function(event) {
+    // Vous pouvez accéder au code de la touche via event.keyCode ou event.key
+    var keyCode = event.keyCode;
+    var key = event.key;
 
-function displayArena() {
+    console.log('Touche enfoncée - Code : ' + keyCode + ', Touche : ' + key);
+    
+    // Exemple : si la touche 'ArrowUp' est enfoncée
+    if (key === 'ArrowUp') {
+        console.log('La touche ArrowUp a été pressée.');
+    } else if (key === 'ArrowDown') {
+        // Faire quelque chose en cas d'appui sur la touche 'ArrowDown'
+        console.log('La touche ArrowDown a été pressée.');
+    }
+});
+
+// V1
+// // Création d'une nouvelle instance de WebSocket en spécifiant l'URL de votre serveur Django
+// const socket = new WebSocket('ws://localhost:8000/challenger/');
+
+// // Événement déclenché lorsqu'une connexion WebSocket est établie
+// socket.onopen = function(event) {
+//     console.log('WebSocket Connected');
+// };
+
+// // Événement déclenché lorsqu'un message est reçu du serveur
+// socket.onmessage = function(event) {
+//     console.log('Message from server:', event.data);
+//     // Traitez le message reçu du serveur selon vos besoins
+// };
+
+// // Fonction pour envoyer un message au serveur
+// function sendMessage(message) {
+//     socket.send(JSON.stringify({"message": message}));
+// }
+
+// function displayArena() {
 	
-}
+// }
